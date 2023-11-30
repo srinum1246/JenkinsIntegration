08019228ff3e6ee8e1d7e4aec494bc8492e1be10
-srinum1246- <-srinum1246@gmail.com->
2023-11-30 16:45:36 +0530

    chrome launch test

--- src/test/java/TestCases/TestngSampleTest.java
+++ src/test/java/TestCases/TestngSampleTest.java
@@ -1,8 +1,21 @@
 package TestCases;
 
-import org.testng.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class TestngSampleTest {
+	
+	WebDriver driver;
 
 	@BeforeSuite
 	public void beforeSuite() {
@@ -21,6 +34,17 @@ public class TestngSampleTest {
 	@BeforeMethod
 	public void beforeMethod() {
 		System.out.println("Before Method");
+//		System.out.println(System.getProperty("user.dir"));
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--allow-insecure-localhost");
+		options.addArguments("--remote-allow-origins=*");
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") +"\\Drivers\\chromedriver.exe");
+		driver=new ChromeDriver(options);
+		driver.get("https://www.google.com");
+		
 	}
 	
 	@Test
@@ -48,6 +72,7 @@ public class TestngSampleTest {
 	@AfterMethod
 	public void AfterMethod() {
 		System.out.println("After Method");
+		driver.quit();
 	}
 	
 	@AfterSuite
